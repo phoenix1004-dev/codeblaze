9a6c882a08340f445724dfd1d7424e65ad1f1a7c
phoenix1004-dev <181405513+phoenix1004-dev@users.noreply.github.com>
2025-01-14 12:21:08 +0700

    make new logo

---
+++ src/components/Logo/Logo2/index.tsx
@@ -0,0 +1,13 @@
+import LogoImage from "./LogoImage";
+import LogoText from "./LogoText";
+
+const Logo = () => {
+  return (
+    <div className="framer-1f9t7pw" data-framer-name="Logo">
+      <LogoImage />
+      <LogoText />
+    </div>
+  );
+};
+
+export default Logo;
