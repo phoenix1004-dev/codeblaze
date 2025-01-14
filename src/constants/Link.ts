aa85a73dd7615407d01ce453b602de99f193a53b
phoenix1004-dev <181405513+phoenix1004-dev@users.noreply.github.com>
2025-01-14 11:26:24 +0700

    add menu link list

---
+++ src/constants/Link.ts
@@ -0,0 +1,10 @@
+import { MenuLink } from "../type";
+
+export const NAV_MENU: MenuLink[] = [
+  { title: "services", link: "./#services" },
+  { title: "process", link: "./#process" },
+  { title: "team", link: "./#team" },
+  { title: "pricing", link: "./#pricing" },
+  { title: "FAQ", link: "./#faq" },
+  { title: "contact", link: "./#contact" },
+];
