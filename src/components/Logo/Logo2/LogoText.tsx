8c72bed9b5d4d361abb07268ea376ab708e45188
phoenix1004-dev <181405513+phoenix1004-dev@users.noreply.github.com>
2025-01-14 13:18:08 +0700

    update second text

--- src/components/Logo/Logo2/LogoText.tsx
+++ src/components/Logo/Logo2/LogoText.tsx
@@ -1,33 +1,33 @@
 const LogoText = () => {
   return (
-    <div className="framer-ckv6z7-container">
+    <div className="framer-1oprh4s-container opacity-100">
       <a
-        className="framer-zMOkf framer-16vruse framer-v-16vruse framer-2s0t08 h-full w-full"
-        data-framer-name="Logo Phone"
+        className="framer-SHlZl framer-qlriss framer-v-qlriss framer-xqedti !h-full !w-full opacity-100 !w-[48px]"
+        data-framer-name="Navigation Logo"
         href="./"
         data-framer-page-link-current="true"
       >
         <div
-          className="framer-awykrg outline-none flex flex-col justify-start flex-shrink-0 transform -translate-x-1/2 -translate-y-1/2 bg-white text-blue-500 underline"
+          className="framer-m6s0ez outline-none flex flex-col justify-start flex-shrink-0 transform -translate-y-1/2 text-[#0099ff] underline"
           data-framer-component-type="RichTextContainer"
         >
-          <p className="framer-text font-[Inter Tight] text-white text-[25px] font-medium tracking-[0px]">
+          <p className="framer-text font-[Switzer] text-white tracking-[0px] translate-y-[2px]">
             <span
               data-text-fill="true"
-              className="framer-text bg-gradient-to-r from-white to-[#70bafa]"
+              className="framer-text bg-white text-[22px]"
             >
               Halo
             </span>
           </p>
         </div>
         <div
-          className="framer-t9ufgm outline-none flex flex-col justify-start flex-shrink-0 transform -translate-x-1/2 bg-white text-[#0099ff] underline"
+          className="framer-c6deyg outline-none flex flex-col justify-start flex-shrink-0 transform -translate-y-1/2 text-[#0099ff] underline !bottom-[-30px]"
           data-framer-component-type="RichTextContainer"
         >
-          <p className="framer-text font-[Inter Tight] text-white text-[25px] font-medium tracking-[0px]">
+          <p className="framer-text font-[Switzer] text-white tracking-[0px] translate-y-[2px]">
             <span
               data-text-fill="true"
-              className="framer-text bg-gradient-to-r from-white to-[#70bafa]"
+              className="framer-text bg-white text-[22px]"
             >
               Halo
             </span>
