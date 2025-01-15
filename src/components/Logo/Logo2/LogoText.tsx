648d71088df1e95af9cd2366c551a81ff022c41a
phoenix1004-dev <181405513+phoenix1004-dev@users.noreply.github.com>
2025-01-15 07:57:00 +0700

    apply the animation to logo text

--- src/components/Logo/Logo2/LogoText.tsx
+++ src/components/Logo/Logo2/LogoText.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+import { HoverMoveTop } from "../../Animation";
+
 const LogoText = () => {
+  const [isActive, setIsActive] = useState<boolean>(false);
   return (
     <div className="framer-1oprh4s-container opacity-100">
       <a
@@ -6,33 +10,39 @@ const LogoText = () => {
         data-framer-name="Navigation Logo"
         href="./"
         data-framer-page-link-current="true"
+        onMouseEnter={() => setIsActive(true)}
+        onMouseLeave={() => setIsActive(false)}
       >
-        <div
-          className="framer-m6s0ez outline-none flex flex-col justify-start flex-shrink-0 transform -translate-y-1/2 text-[#0099ff] underline"
-          data-framer-component-type="RichTextContainer"
-        >
-          <p className="framer-text !font-switzer text-white tracking-[0px] translate-y-[2px]">
-            <span
-              data-text-fill="true"
-              className="framer-text bg-white text-[22px]"
-            >
-              Halo
-            </span>
-          </p>
-        </div>
-        <div
-          className="framer-c6deyg outline-none flex flex-col justify-start flex-shrink-0 transform -translate-y-1/2 text-[#0099ff] underline !bottom-[-30px]"
-          data-framer-component-type="RichTextContainer"
-        >
-          <p className="framer-text font-switzer text-white tracking-[0px] translate-y-[2px]">
-            <span
-              data-text-fill="true"
-              className="framer-text bg-white text-[22px]"
-            >
-              Halo
-            </span>
-          </p>
-        </div>
+        <HoverMoveTop isActive={isActive} from={0} to={-20}>
+          <div
+            className="framer-m6s0ez outline-none flex flex-col justify-start flex-shrink-0 transform -translate-y-1/2 text-[#0099ff] underline"
+            data-framer-component-type="RichTextContainer"
+          >
+            <p className="framer-text !font-switzer text-white tracking-[0px] translate-y-[2px]">
+              <span
+                data-text-fill="true"
+                className="framer-text bg-white text-[22px]"
+              >
+                Halo
+              </span>
+            </p>
+          </div>
+        </HoverMoveTop>
+        <HoverMoveTop isActive={isActive} from={25} to={5}>
+          <div
+            className="framer-c6deyg outline-none flex flex-col justify-start flex-shrink-0 transform -translate-y-1/2 text-[#0099ff] underline !bottom-[-30px]"
+            data-framer-component-type="RichTextContainer"
+          >
+            <p className="framer-text font-switzer text-white tracking-[0px] translate-y-[2px]">
+              <span
+                data-text-fill="true"
+                className="framer-text bg-white text-[22px]"
+              >
+                Halo
+              </span>
+            </p>
+          </div>
+        </HoverMoveTop>
       </a>
     </div>
   );
